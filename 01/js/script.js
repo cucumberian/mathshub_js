ad9f392fece5c3bfbbb9de39c27677331866ddd0
cucumberian <smamarsello@gmail.com>
2023-02-07 13:29:42 +0300

    add: lesson 1 + test 1

---
+++ 01/js/script.js
@@ -0,0 +1,25 @@
+//alert("Сообщение Алерт");   // модальное окно - не дает нам дейстовать пока мы с ним не разберемся
+
+//confirm("Вы учите JavaScript"); // OK - Cancel
+
+// prompt("Как вы учите JS?", "Упорно");
+
+'use strict';
+
+
+const skill = document.getElementById("skill");
+const isLove = document.getElementById("islove");
+const string = document.getElementById('string');
+const types = document.querySelector('#types');
+
+// const skillText = prompt("Какой язык вы учите?", "пока не знаю");
+// const isLoveValue = confirm(`Нравится ли вам ${skillText}?`);
+
+// skill.innerText = skillText;
+// isLove.innerText = isLoveValue ? 'Да' : 'Нет';
+
+string.innerHTML = "str";
+
+console.log(types.innerHTML);
+console.log();
+console.log(types.innerText);
